46190855635765d154c2f5f1f9a0b6f0da4f83de
carmelo-granvillano <melo.granvillano@gmail.com>
2022-04-24 00:02:26 +0200

    add: 100  boxes

--- script/script.js
+++ script/script.js
@@ -1,1 +1,15 @@
 // 100 Rectangles
+// const text = document.createTextNode("Hello");
+// boxes.appendChild(text);
+
+
+
+
+
+
+for (i=0; i<100; i++) {
+    const boxes = document.createElement("div");
+    boxes.classList.add("box")
+    document.getElementById("boxes").appendChild(boxes);
+}
+
