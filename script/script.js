abf0dd09144bf80f111fe1f33fc7d04b30a09039
carmelo-granvillano <melo.granvillano@gmail.com>
2022-04-24 09:55:08 +0200

    filled box

--- script/script.js
+++ script/script.js
@@ -1,15 +1,15 @@
 // 100 Rectangles
-// const text = document.createTextNode("Hello");
-// boxes.appendChild(text);
-
-
-
-
-
 
 for (i=0; i<100; i++) {
-    const boxes = document.createElement("div");
-    boxes.classList.add("box")
-    document.getElementById("boxes").appendChild(boxes);
+    const boxes = document.createElement('div');
+    boxes.classList.add('box')
+    document.getElementById('boxes').appendChild(boxes);
 }
 
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach(box => {
+    box.addEventListener('click', () => {
+        box.classList.add('selected');
+    }) 
+})
