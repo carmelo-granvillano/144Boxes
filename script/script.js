d46ceb319063589654d2dd27901f4b141cc02a8d
carmelo-granvillano <melo.granvillano@gmail.com>
2022-04-24 15:06:42 +0200

    add: selected every 10 minutes

--- script/script.js
+++ script/script.js
@@ -1,13 +1,15 @@
 // 100 Rectangles
 
 // Boxes
-for (i=0; i<100; i++) {
+const numberOfBoxes = 144;
+
+for (i=0; i<numberOfBoxes; i++) {
     const boxes = document.createElement('div');
     boxes.classList.add('box')
     document.getElementById('boxes').appendChild(boxes);
 }
 
-// Selected
+// Selected on click
 const boxes = document.querySelectorAll('.box');
 
 boxes.forEach(box => {
@@ -15,3 +17,30 @@ boxes.forEach(box => {
         box.classList.add('selected');
     }) 
 })
+
+
+// Slected every 10 minutes
+const minSinceMidnight = () => {
+    const now = new Date();
+    const midnight = new Date().setHours(0, 0, 0, 0);
+  
+    return ((now - midnight) / 1000) / 60;
+}
+  
+const fillBox = () => {
+    const minElapsed = minSinceMidnight();
+  
+    const fullBoxes = Math.floor(minElapsed / 10);
+  
+    document.querySelectorAll('.box').forEach((element, index) => {
+      if (index + 1 <= fullBoxes) {
+        element.classList.add('selected');
+      } else {
+        element.classList.remove('selected');
+      }
+    });
+    console.log(minElapsed);
+}
+
+fillBox();
+setInterval(fillBox, 5000);
