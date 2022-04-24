26e519f469edabbe0fba8e6aa3d2aa47f22c66cd
carmelo-granvillano <melo.granvillano@gmail.com>
2022-04-24 10:14:16 +0200

    add: comment

--- script/script.js
+++ script/script.js
@@ -1,11 +1,13 @@
 // 100 Rectangles
 
+// Boxes
 for (i=0; i<100; i++) {
     const boxes = document.createElement('div');
     boxes.classList.add('box')
     document.getElementById('boxes').appendChild(boxes);
 }
 
+// Selected
 const boxes = document.querySelectorAll('.box');
 
 boxes.forEach(box => {
